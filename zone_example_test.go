eb28ea4d1efc19b1bcf920720b3b1679b8309dd5
SublimeDamien <damien@sublimeskinz.com>
2017-11-28 12:32:13 +0100

    Fix the ListZones call

--- zone_example_test.go
+++ zone_example_test.go
@@ -31,7 +31,7 @@ func ExampleAPI_ListZones_filter() {
 	}
 
 	// Fetch a slice of zones example.org and example.net.
-	zones, err := api.ListZones(1, "example.org", "example.net")
+	zones, err := api.ListZones(1)
 	if err != nil {
 		log.Fatal(err)
 	}
